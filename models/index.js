68e7b15efc567fc9336fd8340c201dd231b224c5
Erick Burgos <43017724+ErickGBR@users.noreply.github.com>
2023-11-14 22:15:45 -0600

    fix bad response

--- models/index.js
+++ models/index.js
@@ -2,11 +2,20 @@ const getFBInfo = require("@xaviabot/fb-downloader");
 const { response } = require("express");
 
 const facebookResponse = async(req, res = response) => {
+try {
+    
     const url = req.body.url;    
     const result = await getFBInfo(url)
-    res.json({
+    res.status(200).json({
+        status:true,
         response: result
     });   
+} catch (error) {
+    res.status(500).json({
+        status:false,
+        error: error
+    })
+}
 };
 
 module.exports = {
